340244f71e9202ccb863df08617ca5d219fb2fb3
Egor Levashov <112577182+sxfour@users.noreply.github.com>
2024-01-15 18:03:01 +0300

    Update backup.py

--- backup.py
+++ backup.py
@@ -2,6 +2,6 @@ from packages import BackupData
 
 if __name__ == '__main__':
     # Параметры на класс, folders (Откуда копировать  и куда), to copy (Какие папки копировать)
-    data = BackupData(folders=['C:\\Users\\', r'\\192.168.0.202\Backup'],
+    data = BackupData(folders=['C:\\Users\\', r'\\0.0.0.0\Backup'],
                       to_copy=['Desktop', 'Documents', 'Downloads'],)
     data.makeArchiveFiles()
