e430e2c1a7ab2466a550f5a730f33582555be2a3
Egor Levashov <112577182+sxfour@users.noreply.github.com>
2024-01-22 11:47:37 +0300

    Delete backup.py

--- backup.py
+++
@@ -1,7 +0,0 @@
-from packages import BackupData
-
-if __name__ == '__main__':
-    # Параметры на класс, folders (Откуда копировать  и куда), to copy (Какие папки копировать)
-    data = BackupData(folders=['C:\\Users\\', r'\\0.0.0.0\Backup'],
-                      to_copy=['Desktop', 'Documents', 'Downloads'],)
-    data.makeArchiveFiles()
